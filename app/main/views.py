e6e56614ff398aec88a42bc8e08b90146a8639b5
ueg1990 <uehtesham90@gmail.com>
2016-02-25 00:59:27 -0500

    Define blueprint for main app

---
+++ app/main/views.py
@@ -0,0 +1,25 @@
+from flask import render_template, flash, redirect, session, url_for, request
+from flask.ext.login import current_user, login_required
+
+from . import main
+from ..models import User
+
+
+@main.route('/')
+@main.route('/index')
+def index():
+    user = {'name': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        { 
+            'title': 'Match of the Day', 
+            'url': 'http://www.fullmatchesandshows.com/2016/02/13/bbc-match-of-the-day-week-26-full-show/' 
+        },
+        { 
+            'title': 'Europen Football Show', 
+            'url': 'http://www.fullmatchesandshows.com/2016/02/21/european-football-show-21st-february-2016/' 
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
